4e9ec170c4e0f0f20b0d9004f12342f2de58af91
Amershabou <amershabou@gmail.com>
2019-12-12 16:11:23 -0500

    Update for AWS Proxy

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+httpProxy = require('http-proxy');
 
 
 let app = express();
 const PORT = process.env.PORT || 3000;
+var proxy = httpProxy.createProxyServer({});
 
 
 app.use(express.json());
 app.use(express.static('./'));
+
+
+let services = [{ service: 'product', url: `http://ec2-13-58-162-184.us-east-2.compute.amazonaws.com:3001/` }, { service: 'images', url: 'http://ec2-18-188-250-254.us-east-2.compute.amazonaws.com:3002/' }, { service: 'reviews', url: 'http://ec2-13-58-212-164.us-east-2.compute.amazonaws.com:3003/' }];
+for (let i = 0; i < services.length; i++) {
+
+  app.get(`/${services[i].service}/:pid`, (req, res) => {
+    proxy.web(req, res, { target: services[i].url });
+  });
+}
+
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
